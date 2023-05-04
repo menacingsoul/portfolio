74541b6f36588a17ea107fae58c4817b6541efc0
Adarsh Kumar <98646158+menacingsoul@users.noreply.github.com>
2023-05-04 14:33:06 +0530

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,14 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animation1'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets5.lottiefiles.com/packages/lf20_v9riyrep.json' // lottie file path
+  })
+  var animation2 = bodymovin.loadAnimation({
+    container: document.getElementById('animation2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets2.lottiefiles.com/private_files/lf30_zdkoi7jh.json' // lottie file path
+  })
